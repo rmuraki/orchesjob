7bc53e64909f6462ff64977bf6f58879c3d31ef7
Ryosuke Muraki <ryosuke@mrk.jp>
2026-05-02 21:41:11 +0900

    Fix worng version

--- orchesjob/__init__.py
+++ orchesjob/__init__.py
@@ -2,4 +2,9 @@
 # Copyright (c) 2026 Ryosuke Muraki
 """orchesjob - Lightweight idempotent one-shot job runner."""
 
-__version__ = "1.0.1"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("orchesjob")
+except PackageNotFoundError:
+    __version__ = "unknown"
